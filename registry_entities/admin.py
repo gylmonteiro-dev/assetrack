b0466592e049b22ccddf9242efb0ac4d4ea831b1
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-09-18 11:48:33 -0300

    criado a estrutura de modelos para as classes responsáveis pelo registro das entidades

--- registry_entities/admin.py
+++ registry_entities/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import Organization, Department, Division, Sector
 # Register your models here.
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display= ('id', 'name')
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+@admin.register(Division)
+class ODivisionAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+@admin.register(Sector)
+class SectorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
--- registry_entities/models.py
+++ registry_entities/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Organization(models.Model):
-    name = models.CharField(max_length=255, unique=True, verbose_name='Nome')
+    name = models.CharField(max_length=255, unique=True, verbose_name='Nome da organização')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data de criação')
     update_at = models.DateTimeField(auto_now=True, verbose_name='Data de atualização')
     company_registration_number = models.CharField(max_length=20, null=True, blank=True, verbose_name='CNPJ')
     primary_contact_number = models.CharField(max_length=20, null=True, blank=True, verbose_name='Contato telefônico')
-    primary_contact = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    primary_contact = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Responsável')
 
 
     class Meta:
@@ -16,3 +16,80 @@ class Organization(models.Model):
     def __str__(self) -> str:
         return self.name
 
+
+class Department(models.Model):
+    organization = models.ForeignKey(Organization, on_delete=models.PROTECT, verbose_name='Organização', related_name='departments')
+    name = models.CharField(max_length=255, unique=True, verbose_name="Nome do departamento")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data de criação")
+    update_at = models.DateTimeField(auto_now=True, verbose_name="Data de atualização")
+    company_registration_number = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name="CNPJ"
+    )
+    primary_contact_number = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name="Contato telefônico"
+    )
+    primary_contact = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Responsável",
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Division(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.PROTECT, verbose_name="Departamento", related_name='units')
+    name = models.CharField(max_length=255, unique=True, verbose_name="Nome da unidade")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data de criação")
+    update_at = models.DateTimeField(auto_now=True, verbose_name="Data de atualização")
+    company_registration_number = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name="CNPJ"
+    )
+    primary_contact_number = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name="Contato telefônico"
+    )
+    primary_contact = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Responsável",
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Sector(models.Model):
+    division = models.ForeignKey(Division, on_delete=models.PROTECT, verbose_name='Unidade', related_name='sectors')
+    name = models.CharField(max_length=255, unique=True, verbose_name="Nome do setor")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data de criação")
+    update_at = models.DateTimeField(auto_now=True, verbose_name="Data de atualização")
+    company_registration_number = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name="CNPJ"
+    )
+    primary_contact_number = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name="Contato telefônico"
+    )
+    primary_contact = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Responsável",
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return self.name
