65b41b8769f491d46f8f90a401709a97e6bad0a2
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-09-19 08:53:18 -0300

    criado os modelos para gerenciamento do patrimonio: centro de custo, categoria, ativo, fornecedor e registro do ativo

--- management_patrimony/admin.py
+++ management_patrimony/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from .models import Category, Asset
 # Register your models here.
--- management_patrimony/models.py
+++ management_patrimony/models.py
@@ -1,15 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+from registry_entities.models import Sector
 
 
+TYPE_OPTIONS = (('MOBILE', 'MOVÉL'),
+                   ('IMOBILE', 'IMÓVEL'))
 
-MOVABLE_OR_IMMOVABLE = (('M', 'MOVÉL'),
-                   ('I', 'IMÓVEL'))
 
+CONDITION_OPTIONS = (('NEW', 'NOVO'),
+                     ('USED', 'USADO'),
+                     ('DAMAGED', 'DANIFICADO'))
 
 # Create your models here.
+class CostCenter(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Nome do centro de custo')
+    details = models.TextField(null=True, blank=True, verbose_name='detalhes do centro de custo')
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Nome da categoria')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data do registro')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Data de atualização do registro')
+
+    def __str__(self):
+        return self.name
+
+
 class Asset(models.Model):
+    # sector = models.ForeignKey(Sector, on_delete=models.PROTECT, verbose_name='Setor', related_name='ativos')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Categoria', related_name='ativos')
     name = models.CharField(max_length=255, verbose_name='Nome')
     model = models.CharField(max_length=100, null=True, blank=True, verbose_name='Modelo')
     serial_number = models.CharField(max_length=100, blank=True, null=True, verbose_name='Número de série')
-    price = 
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='Preço')
+    warranty = models.DateField(null=True, blank=True)
+    invoice_number = models.CharField(max_length=100, verbose_name='Nota fiscal')
+    description = models.TextField(null=True, blank=True, verbose_name='Descrição')
+    condition = models.CharField(max_length=10, choices=CONDITION_OPTIONS, default='USED', verbose_name='Condição')
+    type = models.CharField(max_length=10, choices=TYPE_OPTIONS, default='MOBILE')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data do registro")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Data de atualização do registro")
 
+    def __str__(self):
+        return  self.name
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Nome do fornecedor')
+    company_registration_number = models.CharField(max_length=20, null=True, blank=True, verbose_name='CNPJ', default='SEM CADASTRO')
+    person_registration_number = models.CharField(max_length=20, null=True, blank=True, verbose_name='CPF', default='SEM CADASTRO')
+
+    def __str__(self):
+        return self.name
+
+class RegisterAsset(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Usuário', related_name='registros_de_ativos')
+    asset = models.ForeignKey(Asset, on_delete=models.PROTECT, verbose_name='Ativo', related_name='registros_de_ativos')
+    sector = models.ForeignKey(Sector, on_delete=models.PROTECT, verbose_name='Setor', related_name='registro_de_ativos')
+    cost_center = models.ForeignKey(CostCenter, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Centro de custo', related_name='registros_de_ativos' )
+    supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Fornecedor', related_name='registros_de_ativos')
+    amount = models.IntegerField(validators=[MinValueValidator(1, 'Não pode cadastrar com quantidade menor que 1')])
+    observation = models.TextField(null=True, blank=True, verbose_name='Observação')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data de criação')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Data de atualização')
+
+    def __str__(self) -> str:
+        return f"{self.asset}: {self.sector}"
