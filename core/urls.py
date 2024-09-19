2ca1be28f35b28b9028024c135804f8f15175460
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-09-19 10:01:17 -0300

    criado as views com classes do DRF

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('registry_entities.urls'))
+    path('api/v1/', include('registry_entities.urls')),
+    path('api/v1/', include('management_patrimony.urls')),
 ]
--- management_patrimony/views.py
+++ management_patrimony/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView 
+from .models import Asset, Category, CostCenter, RegisterAsset, Supplier
 # Create your views here.
+
+
+class AssetListCreateView(ListCreateAPIView):
+    queryset = Asset.objects.all()
+    serializer_class  = None
+
+
+class AssetRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Asset.objects.all()
+    serializer_class = None
+
+
+class CategoryListCreateView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = None
+
+
+class CategoryRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = None
+
+
+class CostCenterListCreateView(ListCreateAPIView):
+    queryset = CostCenter.objects.all()
+    serializer_class = None
+
+
+class CostCenterRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = CostCenter.objects.all()
+    serializer_class = None
+
+
+class RegisterAssetListCreateView(ListCreateAPIView):
+    queryset = RegisterAsset.objects.all()
+    serializer_class = None
+
+
+class RegisterAssetRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = RegisterAsset.objects.all()
+    serializer_class = None
+
+
+class SupplierAssetListCreateView(ListCreateAPIView):
+    queryset = Supplier.objects.all()
+    serializer_class = None
+
+
+class SupplierRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Supplier.objects.all()
+    serializer_class = None
