a4131cf9184cd120b8e0b936cf6a9679b0a3ff97
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-09-22 17:49:14 -0300

    criado a app de authentication, e inserido nos INSTALLED_APPS. Criado as URL para obter token, atualizar (refresh) e verificar.

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('registry_entities.urls')),
     path('api/v1/', include('management_patrimony.urls')),
+    path('api/v1/', include('authentication.urls'))
 ]
