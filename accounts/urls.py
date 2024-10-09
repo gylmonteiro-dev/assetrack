ab916b55489f9c05db813b70e697843b5dab920c
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-10-09 16:37:51 -0300

    criado um endpoint para recuperar os dados do usuario após a autenticação

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("users/", views.UserCreateApiView.as_view(), name="create-user"),
+    path("user/", views.ProfileUserListView.as_view(), name="get-profile"),
     path(
         "users_profile/<int:pk>/",
         views.UserProfileRetrieveUpdateDestroyView.as_view(),
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from rest_framework.response import Response
 from .serializers import UserModelSerializer, UserProfileSerializar
 from .models import UserProfile
@@ -18,5 +18,16 @@ class UserProfileRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         user = self.request.user
+        queryset = UserProfile.objects.filter(user=user)
+        return queryset
+    
+
+class ProfileUserListView(ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserProfileSerializar
+
+    def get_queryset(self):
+        user = self.request.user
+        print(user)
         queryset = UserProfile.objects.filter(user=user)
         return queryset
