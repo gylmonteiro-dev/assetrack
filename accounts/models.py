cb81dddbc6f4ea5c898d1bd26beba3dd5c4b00d8
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-10-09 10:51:14 -0300

    criado o signal para criar um perfil de usuario mediante ao cadastro de um usuario

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
-
+from django.db import models
+from registry_entities.models import Organization
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='user')
@@ -9,7 +9,7 @@ class UserProfile(models.Model):
     person_registration_number = models.CharField(
         max_length=20, null=True, blank=True, verbose_name="CPF", default="SEM CADASTRO"
     )
-    
+    organization = models.ForeignKey(Organization, on_delete=models.PROTECT, related_name="users_profiles", null=True, blank=True)
 
     def __str__(self):
         return self.user.first_name or self.user.username
