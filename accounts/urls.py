1ec85ed7d5944b8a4420854c2d7a7eeaecf77d79
Gyl Monteiro <gyldeveloper@gmail.com>
2024-10-10 09:49:57 -0300

    modificiado a url para acessar a lista de perfil de usuarios

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path("users/", views.UserCreateApiView.as_view(), name="create-user"),
-    path("user/", views.ProfileUserListView.as_view(), name="get-profile"),
+    path("user_profile/", views.ProfileUserListView.as_view(), name="get-profile"),
     path(
-        "users_profile/<int:pk>/",
+        "user_profile/<int:pk>/",
         views.UserProfileRetrieveUpdateDestroyView.as_view(),
         name="retrieve-update-delete-user_profile",
     ),
